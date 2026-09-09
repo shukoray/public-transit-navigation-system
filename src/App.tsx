052cadbaf0aaba98054a493103dbdeee91c91fb7
GreatMeCloud <zhouray28@gmail.com>
2026-09-08 20:28:58 -0400

    replace test text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
     <div>
-      <h1>Website Title</h1>
-      <p>Test text</p>
+      <h1 onClick={() => window.location.reload()}>
+        Public Transit Navigation System
+      </h1>
+
+      <p>
+        A smart navigation system for public transportation that utilizes real-world map data to optimize routes under simulated traffic conditions. The project explores graph algorithms, dynamic routing, ride-sharing, and efficient transportation planning.
+      </p>
     </div>
   )
 }
